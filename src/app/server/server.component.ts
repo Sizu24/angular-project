0de9fda4697f388374ec006d63ba023cbada3cf7
Sizu24 <sizu1124@gmail.com>
2024-02-21 22:23:31 -0800

    Add two-way-binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,4 +4,22 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html',
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverStatus = 'This servers are at capacity';
+  allowNewServer = true;
+  serverName = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = false;
+    }, 9000);
+  }
+
+  onButtonClickHandler() {
+    this.serverStatus = 'Server ' + this.serverName + ' was created!';
+  }
+
+  onType(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
