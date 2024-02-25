66f7f45e79fe3d0c8a2f3090e8808794a76087e4
Sizu24 <sizu1124@gmail.com>
2024-02-25 13:49:49 -0800

    Add directives and data binding methods

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,19 +7,31 @@ import { Component } from '@angular/core';
 export class ServerComponent {
   serverStatus = 'This servers are at capacity';
   allowNewServer = true;
-  serverName = '';
+  serverName = 'Project Cloud';
+  serverUpdated = false;
+  serverCapacity = '';
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = false;
     }, 9000);
+
+    this.serverCapacity = Math.random() > 0.5 ? 'High' : 'Low';
   }
 
   onButtonClickHandler() {
     this.serverStatus = 'Server ' + this.serverName + ' was created!';
   }
 
+  onUpdateServer() {
+    this.serverUpdated = true;
+  }
+
   onType(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  getColor() {
+    return this.serverCapacity === 'High' ? 'red' : 'green';
   }
 }
